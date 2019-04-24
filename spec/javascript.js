6fe854cdbe2c876ca492005f8347266a5bf5a404
almic <mick.ashton@flare-esports.net>
2019-04-24 15:48:26 -0600

    Update tree-sitter-apidoc

--- spec/javascript.js
+++
@@ -1,8 +0,0 @@
-/**
- * @api {post} /path Some title
- * @apiName This is a name
- * @apiParam (group here) {String{type stuff}} [field="default"] Explanation goes here
- * @apiVersion badVersion1
- * @apiVersion 1.0.10-goodversion
- * @apiSampleRequest https://www.example.com
- */
